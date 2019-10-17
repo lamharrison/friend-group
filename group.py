387e490f65c0d0cb78fea282b2227700ba0413da
lamharrison <lamharrison123@gmail.com>
2019-10-17 11:53:41 +0100

    add group friend

--- group.py
+++ group.py
@@ -2,4 +2,31 @@
 
 # Your code to go here...
 
-my_group =
+
+Jill={
+    'age' : 26,
+    'job': 'biologist',
+    'relastions' : {'Friend':'Zalika', 'Partner' : 'John'}
+}
+
+Zalika={
+    'age' : 28,
+    'job': 'Artist',
+    'relastions' : {'Friend':'Jill'}
+}
+
+John={
+    'age' : 27,
+    'job': 'Writter',
+    'relastions' : {'Partner':'Jill'}
+}
+
+Nash={
+    'age' : 34,
+    'job': 'chef',
+    'relastions' : {'Cousin':'John', 'Landlord':'Zalika'}
+}
+
+my_group =[Jill, Zalika, John, Nash];
+
+print(my_group[0]['relastions']['Friend'])
